63e18aeadf8198f2f1ee2b9db86a8d66b22d4d0c
snibedita92 <nibeditaswain719@gmail.com>
2024-09-28 19:25:47 -0500

    removed target file

--- Week-01/Day-01_Convert-Ages-To-Days/ages_to_days/target/debug/deps/ages_to_days-7637cb484d412436.d
+++
@@ -1,7 +0,0 @@
-/Users/Rashmi/Projects/rustProgramming/100-Days-Of-Rust/Week-01/Day-01_Convert-Ages-To-Days/ages_to_days/target/debug/deps/ages_to_days-7637cb484d412436: src/main.rs src/convert/mod.rs src/convert/age_in_days.rs
-
-/Users/Rashmi/Projects/rustProgramming/100-Days-Of-Rust/Week-01/Day-01_Convert-Ages-To-Days/ages_to_days/target/debug/deps/ages_to_days-7637cb484d412436.d: src/main.rs src/convert/mod.rs src/convert/age_in_days.rs
-
-src/main.rs:
-src/convert/mod.rs:
-src/convert/age_in_days.rs:
