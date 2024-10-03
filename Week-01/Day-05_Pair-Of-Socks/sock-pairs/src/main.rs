e5930fb3acbdd13f3079f3bfb8eb75f8693ff532
snibedita92 <nibeditaswain719@gmail.com>
2024-10-02 23:14:03 -0500

    W1 D5 approach 2

--- Week-01/Day-05_Pair-Of-Socks/sock-pairs/src/main.rs
+++ Week-01/Day-05_Pair-Of-Socks/sock-pairs/src/main.rs
@@ -1,18 +1,19 @@
 use std::collections::HashMap;
 
 fn count_sock_pairs(sequence: &str) -> i32 {
-
-    let mut num_pairs = 0;
-    let mut sock_pairs: HashMap<String, u32> = HashMap::new();
+    let mut sock_pairs: HashMap<String, i32> = HashMap::new();
 
     for ch in sequence.chars(){
         sock_pairs.entry(ch.to_string()).and_modify(|count| *count += 1).or_insert(1);
-        if sock_pairs.get(&ch.to_string()).unwrap() % 2u32 == 0 {
-            num_pairs+=1;
-        }
     }
 
-    num_pairs
+    sock_pairs.values().map(|val| {
+        if val % 2 == 0 {
+            val/2
+        } else {
+            0i32
+        }
+    }).reduce(|acc, v| acc+v).unwrap_or_else(|| 0)
 }
 
 
