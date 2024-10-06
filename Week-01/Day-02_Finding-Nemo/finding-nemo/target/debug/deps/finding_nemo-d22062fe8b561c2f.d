92ab176b85abd192698d929bc6a764050d8d47c3
RashmiRB <rbidanta@yahoo.in>
2024-10-05 21:27:15 -0500

    Removed target and added gitignore

--- Week-01/Day-02_Finding-Nemo/finding-nemo/target/debug/deps/finding_nemo-d22062fe8b561c2f.d
+++
@@ -1,5 +0,0 @@
-/Users/Rashmi/Projects/rustProgramming/100-Days-Of-Rust/Week-01/Day-02_Finding-Nemo/finding-nemo/target/debug/deps/libfinding_nemo-d22062fe8b561c2f.rmeta: src/main.rs
-
-/Users/Rashmi/Projects/rustProgramming/100-Days-Of-Rust/Week-01/Day-02_Finding-Nemo/finding-nemo/target/debug/deps/finding_nemo-d22062fe8b561c2f.d: src/main.rs
-
-src/main.rs:
